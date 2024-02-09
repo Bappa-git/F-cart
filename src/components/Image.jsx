a4ff30a8a9bcba1a37ebd0bb429f5f45db78f0c0
Bappa-git <bappadas09012000@gmail.com>
2024-02-10 00:53:35 +0530

    upload

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -2,7 +2,7 @@ export const imge=[{
    url:"https://rukminim2.flixcart.com/fk-p-flap/1600/270/image/8d4ba438d4496a7b.jpg?q=20"
 },
 {
-    url:"./images/Owlimg2.webp"
+    url:"https://rukminim2.flixcart.com/fk-p-flap/1600/270/image/aa1b2bdcf519b468.jpg?q=20"
 }
 
 ]
