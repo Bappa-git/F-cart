8376c227a2ae52b975ab05e584d8e1cea6276a1d
Bappa-git <bappadas09012000@gmail.com>
2024-02-10 00:38:45 +0530

    upload

---
+++ src/components/Image.jsx
@@ -0,0 +1,3 @@
+export const imge=[{
+   url:"https://rukminim2.flixcart.com/fk-p-flap/1600/270/image/8d4ba438d4496a7b.jpg?q=20"
+}]
--- src/components/Owlcarousel.jsx
+++ src/components/Owlcarousel.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Slider from "react-slick"
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import { items } from './Data';
+import { imge } from './Image';
 
 
 const Owldemo1 = () => {
@@ -18,7 +18,7 @@ const Owldemo1 = () => {
   return (
     <Slider {...settings}>
       <div>
-        <img src="./images/Owlimg1.webp" alt="" width={{width:'100%'}} />
+        <img src={imge[0].url} alt="" width={{width:'100%'}} />
       </div>
       <div>
       <img src="./images/Owlimg2.webp" alt="" width={{width:'100%'}}/>
